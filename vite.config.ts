ec970808fe4c25d36ba1fc0734d51103682278f5
Imants Liepiņš <iliepins01@gmail.com>
2025-07-09 22:02:34 +0300

    Maybe fix base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/invite-web-blossom-21/" : "/",
   server: {
     host: "::",
     port: 8080,
